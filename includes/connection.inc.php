6921ffda65f013fdc7a7aa634d97fc4728ac82e8
Richard nwonah <nwonahr@gmail.com>
2021-08-19 17:04:37 +0100

    added config

--- includes/connection.inc.php
+++ includes/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$db = "CvDb";
+$server = "us-cdbr-east-04.cleardb.com";
+$user = "b0d14bddfc49d5";
+$password = "ca99c80b";
+$db = "heroku_c25248c9bf64005";
 
 
 $conn = new mysqli($server, $user, $password, $db);
